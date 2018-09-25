2b0751d7cea54509cfdf2d57d7ae330d89b67e5d
Archer357 <33368017+Archer357@users.noreply.github.com>
2018-09-24 23:02:26 -0700

    working on socket system

---
+++ Holodeck1/Intermediate/Build/Win64/Holodeck1Editor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "D:/GitHub/PhaserRange/Holodeck1/Intermediate/Build/Win64/Holodeck1Editor/Development/Engine/SharedPCH.Engine.h"
---
+++ Holodeck1/Plugins/VRExpansionPlugin/VRExpansionPlugin/Intermediate/Build/Win64/UE4Editor/Inc/VRExpansionPlugin/GS_GunTools.generated.h
@@ -0,0 +1,126 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VREXPANSIONPLUGIN_GS_GunTools_generated_h
+#error "GS_GunTools.generated.h already included, missing '#pragma once' in GS_GunTools.h"
+#endif
+#define VREXPANSIONPLUGIN_GS_GunTools_generated_h
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_RPC_WRAPPERS \
+ \
+	DECLARE_FUNCTION(execClearRecoil) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		P_THIS->ClearRecoil(); \
+		P_NATIVE_END; \
+	} \
+ \
+	DECLARE_FUNCTION(execAddRecoilInstance) \
+	{ \
+		P_GET_PROPERTY(UFloatProperty,Z_Param_RecoilInstanceStrength); \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		P_THIS->AddRecoilInstance(Z_Param_RecoilInstanceStrength); \
+		P_NATIVE_END; \
+	}
+
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+ \
+	DECLARE_FUNCTION(execClearRecoil) \
+	{ \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		P_THIS->ClearRecoil(); \
+		P_NATIVE_END; \
+	} \
+ \
+	DECLARE_FUNCTION(execAddRecoilInstance) \
+	{ \
+		P_GET_PROPERTY(UFloatProperty,Z_Param_RecoilInstanceStrength); \
+		P_FINISH; \
+		P_NATIVE_BEGIN; \
+		P_THIS->AddRecoilInstance(Z_Param_RecoilInstanceStrength); \
+		P_NATIVE_END; \
+	}
+
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUGS_GunTools(); \
+	friend struct Z_Construct_UClass_UGS_GunTools_Statics; \
+public: \
+	DECLARE_CLASS(UGS_GunTools, UGS_Default, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGS_GunTools)
+
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_INCLASS \
+private: \
+	static void StaticRegisterNativesUGS_GunTools(); \
+	friend struct Z_Construct_UClass_UGS_GunTools_Statics; \
+public: \
+	DECLARE_CLASS(UGS_GunTools, UGS_Default, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGS_GunTools)
+
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UGS_GunTools(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGS_GunTools) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGS_GunTools); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGS_GunTools); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGS_GunTools(UGS_GunTools&&); \
+	NO_API UGS_GunTools(const UGS_GunTools&); \
+public:
+
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGS_GunTools(UGS_GunTools&&); \
+	NO_API UGS_GunTools(const UGS_GunTools&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGS_GunTools); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGS_GunTools); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGS_GunTools)
+
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_PRIVATE_PROPERTY_OFFSET
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_10_PROLOG
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_PRIVATE_PROPERTY_OFFSET \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_RPC_WRAPPERS \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_INCLASS \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_PRIVATE_PROPERTY_OFFSET \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_RPC_WRAPPERS_NO_PURE_DECLS \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_INCLASS_NO_PURE_DECLS \
+	Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Holodeck1_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_GunTools_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
